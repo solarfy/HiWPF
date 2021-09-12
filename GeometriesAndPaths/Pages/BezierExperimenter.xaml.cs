957f167335efc75741935deac1916fac303b8494
Yang <228275230@qq.com>
2021-09-12 17:12:05 +0800

    add GraphicalShapes

---
+++ GeometriesAndPaths/Pages/BezierExperimenter.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GeometriesAndPaths.Pages
+{
+    /// <summary>
+    /// BezierExperimenter.xaml 的交互逻辑
+    /// </summary>
+    public partial class BezierExperimenter : Page
+    {
+        Polyline Bezier;
+
+        public BezierExperimenter()
+        {
+            InitializeComponent();
+            Bezier = new Polyline();
+            Bezier.Stroke = Brushes.OrangeRed;
+            this.canvas.Children.Add(Bezier);
+            this.canvas.SizeChanged += CanvasOnSizeChanged;
+        }
+
+       protected virtual void CanvasOnSizeChanged(object sender, SizeChangedEventArgs args)
+        {
+            this.ptStart.Center = new Point(args.NewSize.Width / 4, args.NewSize.Height / 2);
+            this.ptCtrl1.Center = new Point(args.NewSize.Width / 2, args.NewSize.Height / 4);
+            this.ptCtrl2.Center = new Point(args.NewSize.Width / 2, args.NewSize.Height / 4);
+            this.ptEnd.Center = new Point(args.NewSize.Width / 4, args.NewSize.Height / 2);
+
+            DrawBezierManually();
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(e);
+            Point pt = e.GetPosition(this.canvas);
+
+            if (e.ChangedButton == MouseButton.Left)
+                this.ptCtrl1.Center = pt;
+
+            if (e.ChangedButton == MouseButton.Right)
+                this.ptCtrl2.Center = pt;
+
+            DrawBezierManually();
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            Point pt = e.GetPosition(this.canvas);
+
+            if (e.LeftButton == MouseButtonState.Pressed)
+                this.ptCtrl1.Center = pt;
+
+            if (e.RightButton == MouseButtonState.Pressed)
+                this.ptCtrl2.Center = pt;
+
+            DrawBezierManually();
+        }
+
+        void DrawBezierManually()
+        {
+            Point[] pts = new Point[10];
+
+            for (int i = 0; i < pts.Length; i++)
+            {
+                double t = (double)i / (pts.Length - 1);
+
+                double x = (1 - t) * (1 - t) * (1 - t) * this.ptStart.Center.X +
+                    3 * t * (1 - t) * (1 - t) * this.ptCtrl1.Center.X +
+                    3 * t * t * (1 - t) * this.ptCtrl2.Center.X +
+                    t * t * t * this.ptEnd.Center.X;
+
+                double y = (1 - t) * (1 - t) * (1 - t) * this.ptStart.Center.Y +
+                    3 * t * (1 - t) * (1 - t) * this.ptCtrl1.Center.Y +
+                    3 * t * t * (1 - t) * this.ptCtrl2.Center.Y +
+                    t * t * t * this.ptEnd.Center.Y;
+
+                pts[i] = new Point(x, y);
+            }
+
+            Bezier.Points = new PointCollection(pts);
+        }
+    }
+}
---
+++ TextGeometryDemo/TextGeometry.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+using System.Globalization;
+
+namespace TextGeometryDemo
+{
+    public class TextGeometry
+    {
+        string txt = "";
+        FontFamily fntfam = new FontFamily();
+        FontStyle fntstyle = FontStyles.Normal;
+        FontWeight fntwt = FontWeights.Normal;
+        FontStretch fntstr = FontStretches.Normal;
+        double emsize = 24;
+        Point ptOrigin = new Point(0, 0);
+
+        public string Text
+        {
+            set { txt = value; }
+            get { return txt; }
+        }
+
+        public FontFamily FontFamily
+        {
+            set { fntfam = value; }
+            get { return fntfam; }
+        }
+        
+        public FontStyle FontStyle
+        {
+            set { fntstyle = value; }
+            get { return fntstyle; }
+        }
+
+        public FontWeight FontWeight
+        {
+            set { fntwt = value; }
+            get { return fntwt; }
+        }
+
+        public FontStretch FontStretch
+        {
+            set { fntstr = value; }
+            get { return fntstr; }
+        }
+
+        public double FontSize
+        {
+            set { emsize = value; }
+            get { return emsize; }
+        }
+
+        public Point Origin
+        {
+            set { ptOrigin = value; }
+            get { return ptOrigin; }
+        }
+
+        public Geometry Geometry
+        {
+            get
+            {
+                FormattedText formtxt = new FormattedText(Text
+                    , CultureInfo.CurrentCulture
+                    , FlowDirection.LeftToRight
+                    , new Typeface(FontFamily, FontStyle, FontWeight, FontStretch)
+                    , FontSize
+                    , Brushes.Black);
+
+                return formtxt.BuildGeometry(Origin);
+            }
+        }
+
+        public PathGeometry PathGeometry
+        {
+            get
+            {
+                return PathGeometry.CreateFromGeometry(Geometry);
+            }
+        }
+    }
+}
